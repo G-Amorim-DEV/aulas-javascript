600778fca5670ed6b0db66dd3a9081f995a9686f
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-07-30 17:30:55 -0300

    Arrays
    Entendendo a estrutura do array, como utiliza-lo e como acessar cada índice do array

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,3 +1,17 @@
 'user strict'
 
-console.log("teste!");
+//Declarando array (vetor)
+let esportes = ["natação", "futebol", "tenis-de-mesa", "futsal"];
+
+// Acessando um determinado elemento através do indice
+console.log(esportes[0]);
+
+// Se quiser visualizar a estrutura do ARRAY
+//console.log(esportes);
+
+// O Fernado Scherer foi medalha de bornze nos 50 metros livres na natação nas olimpiadas de Sydney em 1996
+console.log(`O fernando Scherer foi medalha de broze nos 50 metros livres na ${esportes[0]} nas olimpiadas de Sydney em 1996. `);
+
+
+
+
