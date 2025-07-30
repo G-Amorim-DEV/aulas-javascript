1fae1744540d35d2706620a5c27644869d4a86aa
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-07-30 16:25:36 -0300

    Exercício de concatenação ou template literal/string
    Criação de uma pasta JavaScript com um arquivo JS dentro dela, também a criação de um arquivo HTML para conseguir visualizar o JS no console do DevTools. O exercício aborda a utilização de um dos tipos de maneira de concatenar.

---
+++ js/02-meu-script.js
@@ -0,0 +1,19 @@
+/* Exercícios */
+
+// 1) Crie uma variável chamada "uc" e coloque nela o número da UC que estamos
+
+const uc = "UC";
+let qual_uc = 2
+
+// 2) Crie uma variável chamada "competencia" e coloque nela uma frase que reoresente o que estmaos estudando nesta UC.
+let linguagens1, linguagens2, linguagens3;
+
+linguagens1 = 'JavaScript';
+linguagens2 = 'HTML';
+linguagens3 = 'CSS'
+
+let competencia = `Na ${uc}${qual_uc} estamos estudando Front-end, no momento estamos estundando ${linguagens1}, e já estudamos ${linguagens2} e ${linguagens3} `;
+
+// 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: UC2 estudando Front-End com JavaScript.
+console.log(competencia);
+
