734a0f4722212440cecb143eb235f7d04a20c8ae
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-07-30 16:40:31 -0300

    Uso da aspas simples e duplas
    Vendo como utilizar as aspas simples e duplas dentro do console.

--- js/02-meu-script.js
+++ js/02-meu-script.js
@@ -6,14 +6,27 @@ const uc = "UC";
 let qual_uc = 2
 
 // 2) Crie uma variável chamada "competencia" e coloque nela uma frase que reoresente o que estmaos estudando nesta UC.
-let linguagens1, linguagens2, linguagens3;
+let competencia1, linguagens1, linguagens2, linguagens3;
 
+competencia1 = 'Front-End'
 linguagens1 = 'JavaScript';
 linguagens2 = 'HTML';
 linguagens3 = 'CSS'
 
-let competencia = `Na ${uc}${qual_uc} estamos estudando Front-end, no momento estamos estundando ${linguagens1}, e já estudamos ${linguagens2} e ${linguagens3} `;
+let frase = `Na ${uc}${qual_uc} estamos estudando ${competencia1}, no momento estamos estundando ${linguagens1}, e já estudamos ${linguagens2} e ${linguagens3} `;
 
 // 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: UC2 estudando Front-End com JavaScript.
-console.log(competencia);
+console.log(frase);
+
+// ERRADO
+//console.log("Gosto "muito" de paçoca!");
+
+//CORRETO! Aspas simples como comando, e duplas como texto
+console.log('Gosto "muito" de paçoca!');
+
+//CORRETO! Aspas duplas como comando, e simples como texto
+console.log("Gosto 'muito' de paçoca!");
+
+//CORRETO! Escapando as aspas como texto
+console.log("Gosto \"muito\" de paçoca!");
 
