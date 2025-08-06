d8824d4769760e7c2938e5459001d57109fcfeba
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-08-06 17:21:16 -0300

    Desafio - Uso de array e objeto
    O desafio se deu em inserir um array dentro de um objeto e também em inserir um objeto dentro de um objeto.

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -61,5 +61,43 @@ console.log(livros);
 
 console.log(livros[2].titulo);
 
+/* Exercício 4 - Atividade de Arry e Objeto */
+
+/*  1 - Crie um objeto chamado "aluno" contendo os seguintes dados:
+
+- Nome Completo
+- Data de Nascimento
+- (Use ARRAY) Lista de telefones (fixo e celular)
+- (DESAFIO: use um OBJETO nesta propriedade) Endereço contendo (separadamente):
+
+- Rua
+- Número
+- Bairro
+
+*/
+
+/* 2 - Mostre no console o nome do aluno, o telefone celular  eo bairro em que mora. */
+
+const aluno = {
+    nome_completo: "Irineu Noé",
+    data_de_nascimento: "10/05/1978",
+    lista_de_telefones: ["(011)2254-1564", "(011)95684-2578"],
+    endereco: {
+        rua: "Rua Noé da Sua Conta",
+        numero: "001",
+        bairro: "Você Não Sabe Nem EU"
+    }
+
+};
+
+console.log(aluno)
+
+let nome_do_aluno = `O nome do aluno é ${aluno.nome_completo},`
+
+let telefone_celular = ` e seu telefone celular é ${aluno.lista_de_telefones[1]},`
+
+let bairro_que_mora = ` e ele mora no bairro ${aluno.endereco.bairro}.`
+
+console.log(nome_do_aluno + telefone_celular + bairro_que_mora)
 
 
