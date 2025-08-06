36709e01691c088b6260441ea1310b71f5a89af2
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-08-06 14:34:19 -0300

    Revisão dos arrays
    Entendendo como construir um array, ele em JS é uma estrutura heterogênia, assim aceitando todos os tipos de dados.

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -12,6 +12,12 @@ console.log(esportes[0]);
 // O Fernado Scherer foi medalha de bornze nos 50 metros livres na natação nas olimpiadas de Sydney em 1996
 console.log(`O fernando Scherer foi medalha de broze nos 50 metros livres na ${esportes[0]} nas olimpiadas de Sydney em 1996. `);
 
+// Relembrando como criar/usar arrays (vetor)
+// No JS, você pode colocar dados diferentes em um array
+let seila = 6;
+const coisas = [10, "Senac", "<h2>Oie</h2>", seila, 15.88];
 
+console.log(coisas[1])
+console.log(coisas[4])
 
 
