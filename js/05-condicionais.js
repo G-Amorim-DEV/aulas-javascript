192f4154efeca6d6af3920e459b3df5b4ffe1ec2
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-08-19 14:56:44 -0300

    Tipos de Condicionais
    Utilizando as condicionais Simples e as condicionais Compostas.

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -11,10 +11,24 @@ console.log("Exemplo 1");
 let numero = 5;
 
 //Condicional SIMPLES (Usa aoenas o if)
-if(numero >= 10){
+if (numero >= 10) {
     console.log("Se você esta vendo essa mensagem é porque a condição e VERDADEIRA");
-} 
+}
+
+
+console.log("\nExemplo 2");
+
+let aluno = "Ozzy";
+let idade = 18;
+
+/* Lógica: verificar se o aluno é maior ou menor de idade.  */
+
+
+//Condicional COMPOSTA (if/else)
+if (idade >= 18) {
+    console.log(`O aluno ${aluno} é maior de idade!`)
+}
 
 else {
-    console.log("A condição não foi atendida, por isso vendo está mensagem.")
+    console.log(`O aluno ${aluno} é menor de idade!`)
 }
