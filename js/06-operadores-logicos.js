bcdb590ccb3de823ab996b1f919bcd4d50adb666
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-08-19 17:29:15 -0300

    Operador OU/OR e Negação/NOT
    Utilização do Operador ||/OR/OU, como usa-lo e onde usa-lo, e também como é o uso do operador de negação que trabalha como inversor de lógica.

--- js/06-operadores-logicos.js
+++ js/06-operadores-logicos.js
@@ -18,16 +18,15 @@ let idade = 16;
 let temCarta = true;
 
 if (idade >= 18 && temCarta === true){
-    console.log("\nPode dirigir");
+    console.log("\nPode dirigir\n");
 }
 
 else{
-    console.log("\nNão pode dirigir");
+    console.log("\nNão pode dirigir\n");
 }
 
-console.log("Exemplo de uso dos Sinais de Iguais");
 
-
+console.log("\nExemplo de uso dos Sinais de Iguais");
 /* Sobre o duplo e triplo sinal de igual 
 
 - Ao usar == a comparação é somente do VALOR, ou seja, NÃo IMPORTA o tipo de dados (String, number, etc)
@@ -40,5 +39,41 @@ let b = "10";
 //let resultadoComparacao = a == b; TRUE
 let resultadoComparacao = a === b; // FALSE
 
-console.log(resultadoComparacao);
+console.log(`\n${resultadoComparacao}`);
+
+
+console.log("\nExemplo ||");
+// || -> retorna VERDADEIRO se pelo menos uma das condições for VERDADEIRA
+
+let feriado = true;
+let fim_de_semana = true;
+
+// if(feriado || fim_de_semana){
+if(feriado === true || fim_de_semana === true){
+    console.log("\nNão tem aula!");
+}
+else{
+    console.log("\nTem aula!");
+}
+
+
+console.log("\n Exemplo !");
+
+// ! -> Inverte a lógica. O que é TRUE, vira FALSE. O que é FALSE, vira TRUE.
+
+// Não está vigorando
+let black_friday = false;
+
+
+/* Aqui, ao usar ! (NEGAÇÂO), verificamos se a variável NÂO É TRUE. Ou seja, INVERTEMOS a lógica. */
+if(!black_friday){
+    console.log("Preços normais...");  
+}
+
+else{
+    console.log("Preços com descontos...");
+    
+}
+
+
 
