b4ac5ff6c74a73802aeca56ecdd27889adf8af61
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-08-19 16:29:21 -0300

    Condicional Composta/Ternário
    Uso da condiciona Composta com operador ternário, e fim da aula sobre operadores condicionais.

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -32,3 +32,67 @@ if (idade >= 18) {
 else {
     console.log(`O aluno ${aluno} é menor de idade!`)
 }
+
+
+/* 1. Crie duas variáveis conforme a seguir:
+ 
+- Nota 1 (contendo um valor de 0 a 10)
+- Nota 2 (contendo outro valor de 0 a 10)
+ 
+2. Crie uma variável chamada "Média" que receberá o valor CALCULADO da média das duas notas informadas. 
+DICA: você deve SOMAR as duas notas e DEPOIS dividir por 2.
+ 
+3. Programe uma condicional que verifique o valor da média calculada. Se a média for maior/igual a 7, mostre "aprovado". 
+Caso contrário, mostre "reprovado". */
+
+
+/* 1. Criação das variaveis: */
+let nota_1 = 7;
+let nota_2 = 7;
+
+/* 2. Calculando a média: */
+
+let media = (nota_1 + nota_2) / 2;
+
+
+
+/* Verificar se o aluno foi APROVADO ou REPROVADO:*/
+
+if (media >= 7){
+    console.log(`Nota 1 = ${nota_1}\nNota 2 = ${nota_2}\nMédia: ${media}.\nSituação: Aprovado😎😎`);
+}
+
+else{console.log(`Nota 1 = ${nota_1}\nNota 2 = ${nota_2}\nMédia: ${media} \nSituação: Reprovado😡😡`);
+}
+
+
+console.log("\nExemplo 3");
+/* Conforme a média:
+- Média acima de 9? ótimo
+- Média acima de 7? bom
+- Média acima de 5? ruim
+- Média até 5? péssimo
+*/
+
+if ( media > 9){
+    console.log("ÓTIMO!")
+}
+else if ( media > 7){
+    console.log("BOM!")
+}
+else if ( media > 5){
+    console.log("RUIM!")
+}
+else{
+    console.log("PÉSSIMO");
+    
+}
+
+console.log("\nExemplo 4");
+
+/* Condicional COMPOSTA com operador TERNÁRIO?:*/
+
+let situacao = idade >= 18 ? "maior" : "menor";
+
+console.log(situacao);
+
