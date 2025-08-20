4da5a99a9af27b4e87b259ce59853c0a6b7b6d8a
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-08-20 15:01:15 -0300

    Loop FOR com array
    utilizando o loop para acessar o índice no array.

--- js/07-loops.js
+++ js/07-loops.js
@@ -5,13 +5,39 @@ São comandos que, baseados em alguma condição lógica, são progrados para ex
 
 Comandos mais tradicionais: while (enquanto) e for (para)
 
-Normalmento, os loops são controlados através de uma variável contadora.
+Normalmento, os loops são controlados através de uma variável contadora. Esta variaável é comumente chada de "i", "j", "k" ou qualquer outra letra se necessário.
 
 */
+console.log("Exemplo 1: WHILE (enquanto)");
 
-let contador = 1;
-while(contador <= 5){
-    console.log(`valor do contador: ${contador}`);
-    contador++; //incremento
+let i = 1;
+while(i <= 5){
+    console.log(`valor do i: ${i}`);
+    i++; //incremento
+    // i = i + 1
+}
+
+
+console.log("\nExemplo 2: FOR (para)");
+
+/* for( inicialização da variável; condição, atualização) */
+for( let j = 1; j <= 10; j++){
+    console.log(`J vale: ${j}`);
     
+}
+
+console.log("\nExemplo 3: loop regressivo");
+
+for (let k = 10; k >= 1; k--){
+    console.log(`K vale: ${k}`);
+}
+
+console.log("\nExemplo 4: loop e array");
+
+const atividadesFisicas = ["Natação","Futebol", "Caminhada", "Corrida","Musculação", "Ciclismo"];
+
+//Usando loop for tradicional
+for (let i = 0; i < atividadesFisicas.length; i++){
+ console.log(`A atividades física que pode ajudar a sua saúde: ${atividadesFisicas[i]}`);
+  
 }
