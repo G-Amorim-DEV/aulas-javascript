caef240f39e8a51ee2d54daef34fc78cdb733ee9
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-08-20 14:14:04 -0300

    Loops
    Descrição do que é um loop, como ele funciona como usa-lo, e introdução ao loop com while e com variavel contadora.

---
+++ js/07-loops.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/* Loops ou estruturas de petição
+São comandos que, baseados em alguma condição lógica, são progrados para executar ações repetidas vezes.
+
+Comandos mais tradicionais: while (enquanto) e for (para)
+
+Normalmento, os loops são controlados através de uma variável contadora.
+
+*/
+
+let contador = 1;
+while(contador <= 5){
+    console.log(`valor do contador: ${contador}`);
+    contador++; //incremento
+    
+}
