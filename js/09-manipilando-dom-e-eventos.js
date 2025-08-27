881bfdbecf50cbfc1914610095f1c53b9c72f8e0
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-08-27 14:03:43 -0300

    queryselectorAll
    Vendo os tipos de seletores que o JS tem em comum com o CSS

--- js/09-manipilando-dom-e-eventos.js
+++ js/09-manipilando-dom-e-eventos.js
@@ -11,3 +11,8 @@ console.log(mensagem01);
 console.log(pagina);
 
 
+/* Para selecionar varios elementos, use o queryselectorAll.
+O resultado é uma espécie de array de elementos (NodeList) */
+const paragrafo = document.querySelectorAll("p")
+console.log(paragrafo);
+
